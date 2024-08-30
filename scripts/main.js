feb8d88edf5e9da66a489af2151e67ba1574bd4c
david <david.kvasnovsky.1999@gmail.com>
2024-08-30 21:03:37 +0200

    v1.0.6 - addition of multiple certificates and photos

--- scripts/main.js
+++ scripts/main.js
@@ -2,3 +2,6 @@ function hideNavigationMenu()
 {
     document.getElementById("links_visibility_toggle").checked = false;
 }
+Fancybox.bind('[data-fancybox="gallery"]', {
+    // Your custom options for a specific gallery
+});
